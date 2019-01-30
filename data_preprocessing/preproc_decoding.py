cbffc538bb17567310aa56289d808d3f9edb2354
MEGLab <meglab@MEGLabs-iMac-Pro.local>
2019-01-30 15:48:19 +0400

    updates

--- data_preprocessing/preproc_decoding.py
+++ data_preprocessing/preproc_decoding.py
@@ -1,10 +1,12 @@
+import mne
+import os.path as op
 import numpy as np
 from mne.io import read_raw_fif
 from mne import (find_events, Epochs)
 import pandas as pd
 
 # params
-subject = 'A0305'
+subject = 'A0280'
 meg_dir = '/Users/meglab/Desktop/shep_fifs/%s/'%(subject)
 filt_l = 1  # same as aquisition
 filt_h = 60
@@ -13,10 +15,21 @@ tmax = 0.6
 
 # file names
 raw_fname = meg_dir + '%s_shepard-raw.fif'%(subject)
-epochs_fname = meg_dir + '%s_shepard-epo.fif'%(subject_)
+epochs_fname = meg_dir + '%s_shepard-epo.fif'%(subject)
+
+server_dir = '/Volumes/MEG/NYUAD-Lab-Server/DataAnalysis/Shepard/meg/'
 
 # read, filter
-raw = read_raw_fif(raw_fname, preload=True)
+if op.isfile(raw_fname):
+    raw = read_raw_fif(raw_fname, preload=True)
+else:
+    con_fname = server_dir + '%s/%s_shep1_NR.con'%(subject,subject)
+    raw1 = mne.io.read_raw_kit(con_fname, preload=True, slope='+')
+    con_fname = server_dir + '%s/%s_shep2_NR.con'%(subject,subject)
+    raw2 = mne.io.read_raw_kit(con_fname, preload=True, slope='+')
+    raw = mne.concatenate_raws([raw1, raw2])
+    raw.save(raw_fname)
+
 raw = raw.filter(filt_l, filt_h)
 
 # check events, create epochs
